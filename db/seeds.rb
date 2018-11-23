f688806993322ec8ee7ab5a28d3e93c9433d44fe
Maud Gilet <maud.gilet@gmail.com>
2018-11-23 11:26:42 +0100

    new db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Ingredient.destroy_all
+
+spices_attr = []
+fruits_attr = []
+
+5.times do
+  h = {
+    name: Faker::Food.spice
+  }
+  spices_attr << h
+end
+
+5.times do
+  h = {
+    name: Faker::Food.fruits
+  }
+  fruits_attr << h
+end
+
+Ingredient.create(spices_attr)
+Ingredient.create(fruits_attr)
